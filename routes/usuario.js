f78de7db66e7a601975074f8de991975e6358fbf
Ruslan Gonzalez <ruslanguns@gmail.com>
2018-11-10 20:01:04 +0000

    Se ha creado la API para hacer un POST y poder crear un usuario.

--- routes/usuario.js
+++ routes/usuario.js
@@ -6,7 +6,11 @@ var app = express();
 //  Importacion del modelo de usuarios
 var Usuario = require('../models/usuario');
 
-// Rutas
+// Rutas 
+
+// ====================================
+// GET — API Obtener todos los usuarios
+// ==================================== 
 app.get('/', (req, res, next) => {
 
     Usuario.find({}, 'nombre email img role')
@@ -25,9 +29,40 @@ app.get('/', (req, res, next) => {
                     ok: true,
                     usuarios: usuarios
                 });
-
             });
+});
 
+// ====================================
+// POST — API Crear un nuevo usuario
+// ====================================
+app.post('/', (req, res) => {
+
+    var body = req.body;
+
+    var usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: body.password,
+        img: body.img,
+        role: body.role
+    });
+
+    // aqui mostramos como guaradar al usuario...
+    usuario.save((err, usuarioGuardado) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al crear al usuario',
+                errors: err
+            });
+        }
+
+        res.status(201).json({
+            ok: true,
+            usuario: usuarioGuardado
+        });
+    });
 
 
 });
