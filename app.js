7850c8cbb1e76e296248795cc636e0b0eb3922fa
Ruslan Gonzalez <ruslanguns@gmail.com>
2018-11-11 03:21:58 +0000

    Se creÃ³ la API para el LOGIN y la generaciÃn del Token con 4 hrs de navegacion usando la libreria de JWT.
    TambiÃn se corrigiÃ³un problema con la base de datos y unos deprecated
    que habÃan.
    
    Se han configurado los routes, agregando login como nueva ruta y tambien
    se creo todo el POST para poder autenticarnos y validar si todo esta
    bien con el Usuario y ContraseÃa.

--- app.js
+++ app.js
@@ -18,27 +18,29 @@ app.use(bodyParser.json()); // parse application/json
 // Importar rutas
 var appRoutes = require('./routes/app');
 var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 
 // conection a la base de datos
-mongoose.connection.openUri('mongodb://127.0.0.1:27017/hospitalDB', (err, res) => {
+mongoose.set('useCreateIndex', true);
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', { useNewUrlParser: true }, (err, res) => {
 
     if (err) throw err;
 
     console.log(`La base de datos ${ cverde }`, 'Online');
 });
 
-
+// RUTAS
 // midelwhere — se ejecuta antes de que se resuelvan otras rutas
 app.use('/usuario', usuarioRoutes); // se define arriba porque sino se colocarian abajo
+app.use('/login', loginRoutes);
 app.use('/', appRoutes);
 
 
 // color consola
 var cverde = '\x1b[32m%s\x1b[0m';
 
-
 // Escuchar peticiones
 app.listen(3000, () => {
-    console.log(`El servidor Express esta corriendo en el puerto 3000: ${ cverde }`, 'Online');
+    console.log(`El servidor Express esta corriendo en el puerto 3001: ${ cverde }`, 'Online');
 });
---
+++ routes/login.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+// inicializarlo
+var app = express();
+
+//  Importacion del modelo de usuarios
+var Usuario = require('../models/usuario');
+
+
+// ====================================
+// POST Método de login
+// ====================================
+app.post('/', (req, res) => {
+
+    var body = req.body;
+
+    Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuario',
+                errors: err
+            });
+        }
+
+        if (!usuarioDB) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Credenciales incorrectas - email',
+                errors: err
+            });
+        }
+
+        if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Credenciales incorrectas - password',
+                errors: err
+            });
+        }
+
+        // Crear un tocken
+        usuarioDB.password = ':)';
+        var token = jwt.sign({ usuario: usuarioDB }, 'este-es-un-seed-dificil', { expiresIn: 14400 }); // 4 horas
+
+        res.status(200).json({
+            ok: true,
+            usuario: usuarioDB,
+            token: token,
+            id: usuarioDB._id
+        });
+    });
+});
+
+
+
+
+
+module.exports = app;
