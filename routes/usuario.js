85bb00a41be6fadc692ef7faf8c062aec86628d4
Ruslan Gonzalez <ruslanguns@gmail.com>
2018-11-11 00:55:11 +0000

    Se encripto el campo password con bcryptjs

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bcrypt = require('bcryptjs');
 
 // inicializarlo
 var app = express();
@@ -42,7 +43,7 @@ app.post('/', (req, res) => {
     var usuario = new Usuario({
         nombre: body.nombre,
         email: body.email,
-        password: body.password,
+        password: bcrypt.hashSync(body.password, 10),
         img: body.img,
         role: body.role
     });
