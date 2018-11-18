2fbd67ca519e26f9be6ce5b0cad6b9e9aa61fe77
Ruslan Gonzalez <ruslanguns@gmail.com>
2018-11-18 12:34:52 +0000

    Commit: Se agregó soporte de carga de imagen y visualización de ellas.

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ var hospitalRoutes = require('./routes/hospital');
 var medicoRoutes = require('./routes/medico');
 var busquedaRoutes = require('./routes/busqueda');
 var uploadRoutes = require('./routes/upload');
+var imagenesRoutes = require('./routes/imagenes');
 
 
 // conection a la base de datos
@@ -52,6 +53,7 @@ app.use('/hospital', hospitalRoutes);
 app.use('/medico', medicoRoutes);
 app.use('/busqueda', busquedaRoutes);
 app.use('/upload', uploadRoutes);
+app.use('/img', imagenesRoutes);
 
 app.use('/', appRoutes);
 
